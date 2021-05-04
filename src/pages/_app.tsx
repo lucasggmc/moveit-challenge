12a690782a85e7aefcea2bc649823ef4c4adf338
lucasggmc <lucasggmc@gmail.com>
2021-05-04 13:57:17 -0300

    hotfix remove menu of login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
-import '../styles/global.css'
-
+import { useRouter } from 'next/router'
 import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
 import { VerticalMenu } from '../components/VerticalMenu'
 
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const route = useRouter();
+  const pathname = '/login'
+
    return (
   //   <ChallengesProvider>
       <>
+      {
+        route.pathname != pathname ? 
+        (
         <VerticalMenu />
+        )
+        :
+        (
+          <div />
+        )
+      }
         <Component {...pageProps} />
       </>
     // </ChallengesProvider>
